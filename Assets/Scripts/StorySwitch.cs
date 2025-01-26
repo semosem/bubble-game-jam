3cbf51cd2f0059056d3f54aa08d55798a00bed1f
Sercan Altundas <sercan.altundas@gmail.com>
2025-01-26 13:49:52 +0200

    menu bg

--- Assets/Scripts/StorySwitch.cs
+++ Assets/Scripts/StorySwitch.cs
@@ -1,7 +1,7 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class StorySwitch : MonoBehaviour
 {
