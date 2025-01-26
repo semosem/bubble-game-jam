b45ee365870c555706faa3757985b135915df0b0
Sercan Altundas <sercan.altundas@gmail.com>
2025-01-26 15:19:03 +0200

    all assets and menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,13 +6,21 @@ public class Menu : MonoBehaviour
 {
     [SerializeField] private Button startButton;
     [SerializeField] private Button exitButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private GameObject creditsPanel;
 
     private void Start()
     {
         startButton.onClick.AddListener(StartGame);
         exitButton.onClick.AddListener(ExitGame);
+        creditsButton.onClick.AddListener(ToggleCredits);
     }
-    
+
+    private void ToggleCredits()
+    {
+        creditsPanel.SetActive(!creditsPanel.activeSelf);
+    }
+
     private void ExitGame()
     {
         Application.Quit();
